4de8d339634c8374d0907587c7f952c61d0dd48f
AppleTrick <arrans9591@gmail.com>
2021-06-10 11:54:38 +0900

    숫자야구 , import와 require q, 반복문에 관하여

---
+++ 05NumberBaseBall/NumberBaseball.jsx
@@ -0,0 +1,61 @@
+import React , { Component } from 'react';
+// 합치는 게 가능const { Component } = React;
+
+function getNumbers() { // 숫자 4개를 랜덤하게 뽑는 함수
+
+}
+
+class NumberBaseball extends Component{
+    state = {
+        result : '',
+        value : '',
+        answer : getNumbers(),
+        tries : [],
+    };
+
+    onSubmitForm = () => {
+
+    };
+
+    onChangeInput = () =>{
+
+    }
+    
+    render(){
+        return(
+            <>
+                <h1>{this.state.result}</h1>
+                <form onSubmit={this.onSubmitForm}>
+                    <input maxLength={4} value={this.state.value} onChange={this.onChangeInput}/>
+                    {/* <input maxLength={4} defaultValue = {this.state.value}> */}
+                </form>
+                <div>시도 : {this.state.tries.length}</div>
+                <ul>
+                    {/* 배열형식 */}
+                    {/* { ['사과','배','바나나','포도'].map((v) => {
+                        return (
+                            <li>{v}</li>
+                        );
+                    }) } */}
+
+                    {/* 오브젝트 형식 */}
+                    {[
+                        { fruit : '사과', taste : '맛있다'},
+                        { fruit : '감', taste : '떫다'},
+                        { fruit : '귤', taste : '시다'},
+                        { fruit : '배', taste : '달다다'},
+                    ].map( (v) =>  (
+                            <li key={v.fruit + v.taste}><b>{v.fruit}</b> - {v.taste}</li>
+                        )
+                    )}
+
+
+                </ul>
+            </>
+        )
+    }
+}
+
+// default는 하나만 가능
+export default NumberBaseball;  // import NumberBaseball
+// module.exports 는 엄밀히는 다르다.
